beef0d954c81ccd21a07e6185dc067cbfec9e952
Simeon Duwel <simeon.duwel@student.kuleuven.be>
2024-12-09 20:02:26 +0100

    completion of day 9

--- 09/aoc2024-09.hs
+++ 09/aoc2024-09.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE LambdaCase #-}
 
-import Data.List (findIndex)
+import Data.List (findIndex, iterate')
 import Data.Maybe (fromJust, isNothing, mapMaybe)
+import Data.Sequence (Seq (..))
+import Data.Sequence qualified as Seq
 import System.Environment (getArgs)
 
 step1 :: [Maybe Int] -> [Maybe Int]
@@ -19,22 +21,25 @@ isVoid :: Span -> Bool
 isVoid (Void _) = True
 isVoid _ = False
 
-step2 :: [Span] -> [Span]
-step2 [] = []
-step2 [a] = [a]
-step2 d = case last d of
-  v@(Void n) -> step2 (init d) ++ [v]
-  f@(File ln idx) -> case findIndex (\case Void n -> n >= ln; _ -> False) d of
-                    Nothing -> step2 (init d) ++ [f]
-                    Just pos -> let voidLen = case d !! pos of Void n -> n; _ -> error "found File which should be Void"
-                                 in take pos d ++ [f] ++ [Void $ voidLen - ln | voidLen > ln] ++ init (drop (pos + 1) d) ++ [Void ln]
-                                 
+step2 :: Seq Span -> Seq Span
+step2 Empty = Empty
+step2 (a :<| Empty) = a :<| Empty
+step2 d =
+  let (initial Seq.:> l) = Seq.viewr d
+   in case l of
+        v@(Void n) -> step2 initial Seq.|> v
+        f@(File ln idx) -> case Seq.findIndexL (\case Void n -> n >= ln; _ -> False) d of
+          Nothing -> step2 initial Seq.|> f
+          Just pos ->
+            let voidLen = case d `Seq.index` pos of Void n -> n; _ -> error "found File which should be Void"
+                (remainder Seq.:> _) = Seq.viewr (Seq.drop (pos + 1) d)
+             in ((Seq.take pos d Seq.|> f) Seq.>< (if voidLen > ln then Seq.singleton (Void $ voidLen - ln) else Seq.empty) Seq.>< remainder) Seq.|> Void ln
 
 showSpan :: Span -> String
 showSpan (Void n) = replicate n '.'
-showSpan (File n i) = concat $ replicate n (show i)
+showSpan (File n i) = concat $ replicate n ("[" ++ show i ++ "]")
 
-showDisc :: [Span] -> String
+showDisc :: Seq Span -> String
 showDisc = concatMap showSpan
 
 main :: IO ()
@@ -49,16 +54,18 @@ main = do
   let fp1 = fromJust $ findFirstRep (iterate step1 disc1)
   let pt1 = sum $ zipWith (*) (map fromJust fp1) [0 ..]
 
---   putStrLn $ "part 1: " ++ show pt1
+  --   putStrLn $ "part 1: " ++ show pt1
 
   let disc2 =
         let spacings = zipWith (\f v -> f v) (cycle [Right, Left]) nums
-         in zipWith (\s idx -> either Void (`File` idx) s) spacings (map (`div` 2) [0 ..])
-  
-  mapM_ (putStrLn . showDisc) (iterate step2 disc2)
-  let fp2 = fromJust $ findFirstRep (iterate step2 disc2)
-  let fp2AsNums = concatMap (\case Void n -> replicate n 0; File n idx -> replicate n idx) fp2
-  let pt2 = sum $ zipWith (*) fp2AsNums [0..]
-  return ()
---   putStrLn $ "part 2: " ++ show pt2
+         in Seq.fromList $ zipWith (\s idx -> either Void (`File` idx) s) spacings (map (`div` 2) [0 ..])
 
+  let chsm l = sum $ zipWith (*) (concatMap (\case Void n -> replicate n 0; File n idx -> replicate n idx) l) [0 ..]
+
+  mapM_ (print . chsm) (iterate' step2 disc2)
+  let fp2 = fromJust $ findFirstRep (iterate' step2 disc2)
+  let fp2AsNums = concatMap (\case Void n -> replicate n 0; File n idx -> replicate n idx) fp2
+  let pt2 = sum $ zipWith (*) fp2AsNums [0 ..]
+
+  print $ length $ showDisc disc2
+  putStrLn $ "part 2: " ++ show pt2
